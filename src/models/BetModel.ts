c565122be1ec759bee240da66538fb8953b79d64
hardikshah18 <hshah366@gmail.com>
2025-04-01 10:41:57 -0400

    Update with UI and CSS

--- src/models/BetModel.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const BetSchema = new mongoose.Schema({
-  player: String,
-  bet: Object,
-  createdAt: Date
-});
-
-export const BetModel = mongoose.model("Bet", BetSchema);
